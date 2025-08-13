9364132d20d75cf7ac0f4c57f360f0d17eec1137
Fernando Mendoza V <f3rm3ndouza@outlook.com>
2025-08-13 03:21:41 +0000

    Add exe option to choose tiled, naive or both

--- src/main.c
+++ src/main.c
@@ -1,84 +1,106 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "matrix.h"
 
 #define EXPECTED_ARGC 4
 
-void perform_benchmark(int n, int num_iterations, int tile_size);
+void benchmark_naive(int n, int num_iterations);
+void benchmark_tiled(int n, int tile_size, int num_iterations);
 
 int main(int argc, char* argv[])
 {
     int n, num_iterations, tile_size;
 
-    if (argc != EXPECTED_ARGC)
+    if (argc < EXPECTED_ARGC)
     {
-        printf("Usage: mmperf <n> <num_iterations> <tile_size>\n");
+        printf("Usage: matrix_mult <both|naive|tiled> <n> <num_iterations> [tile_size]\n");
         exit(1);
     }
 
-    n = atoi(argv[1]);
-    num_iterations = atoi(argv[2]);
-    tile_size = atoi(argv[3]);
+    n = atoi(argv[2]);
+    num_iterations = atoi(argv[3]);
 
-    printf("n = %i, tile size = %i\n", n, tile_size);
+    printf("Matrix multiply with n = %i, %i iterations\n", n, num_iterations);
 
-    perform_benchmark(n, num_iterations, tile_size);
+    if (0 == strcmp(argv[1], "both") || 0 == strcmp(argv[1], "naive"))
+    {
+        printf("Naive multiply\n");
+        benchmark_naive(n, num_iterations);
+    }
+
+    if (0 == strcmp(argv[1], "both") || 0 == strcmp(argv[1], "tiled"))
+    {
+        tile_size = atoi(argv[4]);
+        printf("Tiled multiply, tile size = %i\n", tile_size);
+        benchmark_tiled(n, tile_size, num_iterations);
+    }
+
+    printf("Done\n");
 
     return 0;
 }
 
-void perform_benchmark(int n, int num_iterations, int tile_size)
+void benchmark_naive(int n, int num_iterations)
 {
-    struct square_matrix a, b, result, result_aware;
+    struct square_matrix a, b, result;
     float temp;
 
-    // Create and initialize matrices with dummy values.
+    // Create and initialize matrices with placeholder values.
     square_matrix_create(n, &a);
     square_matrix_create(n, &b);
     square_matrix_create(n, &result);
-    square_matrix_create(n, &result_aware);
-    
+
     temp = 1.0f;
     for (int i = 0; i != a.n; ++i)
     {
         for (int j = 0; j != a.n; ++j)
         {
-            a.entries[i][j] = temp;
+            a.entries[i][j] = temp - 0.1f;
+            b.entries[i][j] = temp + 0.3f;
             temp += 1.0f;
         }
     }
 
-    temp = 1.0f;
-    for (int i = 0; i != b.n; ++i)
-    {
-        for (int j = 0; j != b.n; ++j)
-        {
-            b.entries[i][j] = temp;
-            temp += 0.8f;
-        }
-    }
-
-    // Benchmark naive implementation.
     for (int i = 0; i != num_iterations; ++i)
     {
         zeros(&result);
         naive_multiply(&a, &b, &result);
     }
 
-    // Benchmark tiled implementation.
-    for (int i = 0; i != num_iterations; ++i)
+    square_matrix_delete(&a);
+    square_matrix_delete(&b);
+    square_matrix_delete(&result);
+}
+
+void benchmark_tiled(int n, int tile_size, int num_iterations)
+{
+    struct square_matrix a, b, result;
+    float temp;
+
+    // Create and initialize matrices with placeholder values.
+    square_matrix_create(n, &a);
+    square_matrix_create(n, &b);
+    square_matrix_create(n, &result);
+
+    temp = 1.0f;
+    for (int i = 0; i != a.n; ++i)
     {
-        zeros(&result_aware);
-        tiled_multiply(tile_size, &a, &b, &result_aware);
+        for (int j = 0; j != a.n; ++j)
+        {
+            a.entries[i][j] = temp - 0.1f;
+            b.entries[i][j] = temp + 0.3f;
+            temp += 1.0f;
+        }
     }
 
-    // Display result.
-    printf("Done, %i iterations for n = %i\n", num_iterations, n);
-    // Sanity check to verify approaches produce equal results.
-    printf("Results match? %s\n", are_matrices_equal(&result, &result_aware) ? "yes" : "no");
+    for (int i = 0; i != num_iterations; ++i)
+    {
+        zeros(&result);
+        tiled_multiply(tile_size, &a, &b, &result);
+    }
 
     square_matrix_delete(&a);
     square_matrix_delete(&b);
     square_matrix_delete(&result);
-    square_matrix_delete(&result_aware);
 }
